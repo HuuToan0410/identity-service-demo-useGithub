0b0489a5c0760b48fc98f422b6c8132e4af7343a
Hứa Hữu Toàn <toanhh0410@gmail.com>
2025-06-22 14:40:42 +0700

    update getUser

--- src/main/java/com/backend/identity_service/Controller/UserController.java
+++ src/main/java/com/backend/identity_service/Controller/UserController.java
@@ -1,6 +1,9 @@
 package com.backend.identity_service.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.backend.identity_service.DTO.UserCreationRequest;
 import com.backend.identity_service.entity.User;
 import com.backend.identity_service.service.UserService;
+
 
 
 
@@ -19,7 +23,13 @@ public class UserController {
     @PostMapping("/users")
     User creatUser(@RequestBody UserCreationRequest request) {
         return userService.createUser(request);
-        
+
     }
+    
+    @GetMapping("/users")
+    List<User> getUSers() {
+        return userService.getUsers();
+   }
+    
     
 }
--- src/main/java/com/backend/identity_service/service/UserService.java
+++ src/main/java/com/backend/identity_service/service/UserService.java
@@ -1,5 +1,7 @@
 package com.backend.identity_service.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +25,11 @@ public class UserService {
         user.setDob(request.getDob());
 
         return userRepository.save(user);
-        
+
     }
+    
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
 }
