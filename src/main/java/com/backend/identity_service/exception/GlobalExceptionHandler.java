1790d8b9203f4567d9767dae73839927d71f25aa
Hứa Hữu Toàn <toanhh0410@gmail.com>
2025-06-22 15:44:53 +0700

    add GlobalExceptionHandler to handle RuntimeExceptions

---
+++ src/main/java/com/backend/identity_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.backend.identity_service.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = RuntimeException.class)
+    ResponseEntity<String> handlingRunTimeException(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+        
+    }
+    
+}
