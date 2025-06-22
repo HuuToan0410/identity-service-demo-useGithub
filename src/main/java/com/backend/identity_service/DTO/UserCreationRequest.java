af8f4663b9c24f57788672dead7b4746d658c2db
Hứa Hữu Toàn <toanhh0410@gmail.com>
2025-06-22 16:28:34 +0700

    add validation annotation for username length in UserCreationRequest

--- src/main/java/com/backend/identity_service/DTO/UserCreationRequest.java
+++ src/main/java/com/backend/identity_service/DTO/UserCreationRequest.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import jakarta.validation.constraints.Size;
 
 public class UserCreationRequest {
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters long")
     private String username;
 
     @Size(min = 8, message = "Password must be at least 8 characters long")
